35c9431ec361c6ba40f7998192e6f100fd9f1c37
AdvancedCoding <AdvancedCoding@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-23 13:28:50 +0000

    css fixing
    git-svn-id: https://plugins.svn.wordpress.org/woodiscuz-woocommerce-comments/trunk@1031152 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpc-css.php
+++ wpc-css.php
@@ -17,12 +17,8 @@ class WPC_CSS {
     public function init_styles() {
         ?>
         <style type="text/css">
-
             #woopcomm form .item {display: block;} .item input {}
-
-            /* ===== Comment Form CSS ===== */
-
-            #woopcomm { margin:10px auto; width:99%; padding:15px; border:#CCCCCC dotted 1px; }
+			#woopcomm { margin:10px auto; width:99%; padding:15px; border:#CCCCCC dotted 1px; }
             #woopcomm form { margin:0px; padding:0px; background:none; border:none; }
             #woopcomm form div{ margin:0px; }
             #woopcomm .wpc-form-wrapper{ padding:10px; background:#F9F9F9; margin-top:10px; border:1px solid #F1F1F1; }
@@ -71,75 +67,21 @@ class WPC_CSS {
             #woopcomm .wpc-toggle{ float:right; text-align:right; padding-right:0px; margin-right:0px; color:#999999; cursor:pointer; font-size:12px; }
             #woopcomm .item { background: none; border-radius: 0px; box-shadow: none; }
             #wpc_response_info img{ margin: 0px auto 0px auto; }
-            #woopcomm .share_buttons_box img { display:inline!important; }
+            #woopcomm .share_buttons_box img { display:inline!important; width:16px; height:16px; }
             #woopcomm .wpc-captcha-label img{ display: inline!important; border:none; padding:0px 1px; margin:0px; }
-
-            /* === CUSTOM === */
-
-            #woopcomm .wpc-reply-link,
-            #woopcomm .wpc-vote-link,
-            #woopcomm .wpc-share-link {
-                cursor: pointer; font-size:13px; font-weight:bold; color: <?php echo $this->highlight_color; ?>;
-            }
-
-            #woopcomm .wpc-form-footer,
-            #woopcomm .wpc-secondary-forms-wrapper {display: none;}
-
-            #woopcomm .wpc-field-captcha .wpc-captcha-label {
-                margin-left: 5px;
-                padding: 0;
-                display: inline-block;
-            }
-
-            #woopcomm .wpc_captcha_refresh_img {cursor: pointer; margin-left: 3px;}
+			#woopcomm .wpc-reply-link, #woopcomm .wpc-vote-link, #woopcomm .wpc-share-link { cursor: pointer; font-size:13px; font-weight:bold; color: <?php echo $this->highlight_color; ?>; }
+			#woopcomm .wpc-form-footer, #woopcomm .wpc-secondary-forms-wrapper {display: none;}
+			#woopcomm .wpc-field-captcha .wpc-captcha-label { margin-left: 5px; padding: 0; display: inline-block; }
+			#woopcomm .wpc_captcha_refresh_img {cursor: pointer; margin-left: 3px;}
             #woopcomm .share_buttons_box {display: none;/*position: absolute;left: 40%;*/}
-
-            #woopcomm .wpc-no-left-margin {margin-left: 0 !important;}
-
-            div.wpc_modal {
-                background: none repeat scroll 0 0 #EDEDED;
-                color:#444444;
-                font-size: 18px;
-                font-weight: normal;
-                padding: 45px 10px 50px 10px!important;
-                text-align: center;
-                line-height:25px;
-            }
-
-            /* ===== Comment Form CSS ===== */
-
-            .wpc-load-more-submit-wrap {
-                width: 100%;
-                text-align: center;
-            }
-
-            .wpc-load-more-submit {
-                width: 100%;
-                text-align: center;
-            }
-
-            #wpc_openModalFormAction > div#wpc_response_info {
-                width: 200px;
-                background: none repeat scroll 0 0 #EDEDED;
-                color:#444444;
-                font-size: 18px;
-                font-weight: normal;
-                padding: 45px 10px 50px 10px!important;
-                text-align: center;
-                line-height:25px;
-            }
-
-            #wpc_openModalFormAction > div#wpc_response_info {
-                /*z-index: 10000;*/
-            }
-            
-            #wpc_openModalFormAction > div#wpc_response_info a.close {
-              background: url("<?php echo plugins_url(WPC::$PLUGIN_DIRECTORY . '/files/img/x.png'); ?>") no-repeat;
-              background-position-x: right;
-              background-position-y: top;
-            }
-
-        </style>
+			#woopcomm .wpc-no-left-margin {margin-left: 0 !important;}
+			div.wpc_modal { background: none repeat scroll 0 0 #EDEDED; color:#444444; font-size: 18px; font-weight: normal; padding: 45px 10px 50px 10px!important; text-align: center; line-height:25px;}
+            .wpc-load-more-submit-wrap {  width: 100%; text-align: center; }
+            .wpc-load-more-submit { width: 100%; text-align: center; }
+            #wpc_openModalFormAction > div#wpc_response_info { width: 200px; background: none repeat scroll 0 0 #EDEDED; color:#444444; font-size: 18px; font-weight: normal; padding: 45px 10px 50px 10px!important; text-align: center; line-height:25px;}
+			#wpc_openModalFormAction > div#wpc_response_info { /*z-index: 10000;*/ }
+            #wpc_openModalFormAction > div#wpc_response_info a.close {  background: url("<?php echo plugins_url(WPC::$PLUGIN_DIRECTORY . '/files/img/x.png'); ?>") no-repeat;  background-position-x: right; background-position-y: top; }
+		</style>
         <?php
     }
 
