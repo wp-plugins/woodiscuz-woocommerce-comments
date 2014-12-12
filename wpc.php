5b258c8795abd9140088b1631dad7682730edca4
AdvancedCoding <AdvancedCoding@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 22:52:56 +0000

    change js version
    git-svn-id: https://plugins.svn.wordpress.org/woodiscuz-woocommerce-comments/tags/1.1.2@1043762 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpc.php
+++ wpc.php
@@ -218,7 +218,7 @@ class WPC {
         wp_register_style('validator-style', plugins_url(WPC::$PLUGIN_DIRECTORY . '/files/css/fv.css'));
         wp_enqueue_style('validator-style');
 
-        wp_enqueue_script('wpc-ajax-js', plugins_url(WPC::$PLUGIN_DIRECTORY . '/files/js/wpc-ajax.js'), array('jquery'), '1.1.1', false);
+        wp_enqueue_script('wpc-ajax-js', plugins_url(WPC::$PLUGIN_DIRECTORY . '/files/js/wpc-ajax.js'), array('jquery'), '1.1.2', false);
         wp_localize_script('wpc-ajax-js', 'wpc_ajax_obj', array('url' => admin_url('admin-ajax.php')));
 
         wp_enqueue_script('wpc-cookie-js', plugins_url(WPC::$PLUGIN_DIRECTORY . '/files/js/jquery.cookie.js'), array('jquery'), '1.4.1', false);
